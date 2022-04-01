150d7cd429b069ccbfeeff0ff0488297b694f69e
Jennifer Gifford <jgifford82@gmail.com>
2022-03-31 18:37:50 -0600

    add bio section in index.html

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
 
 // const h1 = document.getElementById("greeting");
-// function mouseOverAlert() {
+// function mouseOverAlert(greeting) {
 //     alert("Huzzah!");
 //     h1.addEventListener("mouseover", mouseOverAlert);
 // }
+
+// const h1 = document.getElementById("greeting");
+// h1.addEventListener('mouseover', 'Huzzah!');
